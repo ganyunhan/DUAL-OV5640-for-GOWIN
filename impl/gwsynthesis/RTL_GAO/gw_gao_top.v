2d5181d3214622f90321698e8a64ffc0f2d87b4c
Baymaxly <288338@whut.edu.cn>
2021-11-02 16:33:58 +0800

    增加拼接功能，像素点部分缺失，垂直未对其:rocket:

--- impl/gwsynthesis/RTL_GAO/gw_gao_top.v
+++ impl/gwsynthesis/RTL_GAO/gw_gao_top.v
@@ -66,36 +66,9 @@ module gw_gao(
     \u_cmos_add/cmos0_delay[1] ,
     \u_cmos_add/cmos0_delay[0] ,
     \u_cmos_add/cmos0_href_reg ,
-    \u_cmos_add/R[4] ,
-    \u_cmos_add/R[3] ,
-    \u_cmos_add/R[2] ,
-    \u_cmos_add/R[1] ,
-    \u_cmos_add/R[0] ,
-    \u_cmos_add/G[5] ,
-    \u_cmos_add/G[4] ,
-    \u_cmos_add/G[3] ,
-    \u_cmos_add/G[2] ,
-    \u_cmos_add/G[1] ,
-    \u_cmos_add/G[0] ,
-    \u_cmos_add/B[4] ,
-    \u_cmos_add/B[3] ,
-    \u_cmos_add/B[2] ,
-    \u_cmos_add/B[1] ,
-    \u_cmos_add/B[0] ,
     \u_cmos_add/cmos0_vsync ,
     \u_cmos_add/cmos1_vsync ,
     \u_cmos_add/pixel_vsync ,
-    \u_cmos_add/pixel_flip_flag ,
-    \u_cmos_add/pixel_href_cnt[9] ,
-    \u_cmos_add/pixel_href_cnt[8] ,
-    \u_cmos_add/pixel_href_cnt[7] ,
-    \u_cmos_add/pixel_href_cnt[6] ,
-    \u_cmos_add/pixel_href_cnt[5] ,
-    \u_cmos_add/pixel_href_cnt[4] ,
-    \u_cmos_add/pixel_href_cnt[3] ,
-    \u_cmos_add/pixel_href_cnt[2] ,
-    \u_cmos_add/pixel_href_cnt[1] ,
-    \u_cmos_add/pixel_href_cnt[0] ,
     sys_clk,
     tms_pad_i,
     tck_pad_i,
@@ -170,36 +143,9 @@ input \u_cmos_add/cmos0_delay[2] ;
 input \u_cmos_add/cmos0_delay[1] ;
 input \u_cmos_add/cmos0_delay[0] ;
 input \u_cmos_add/cmos0_href_reg ;
-input \u_cmos_add/R[4] ;
-input \u_cmos_add/R[3] ;
-input \u_cmos_add/R[2] ;
-input \u_cmos_add/R[1] ;
-input \u_cmos_add/R[0] ;
-input \u_cmos_add/G[5] ;
-input \u_cmos_add/G[4] ;
-input \u_cmos_add/G[3] ;
-input \u_cmos_add/G[2] ;
-input \u_cmos_add/G[1] ;
-input \u_cmos_add/G[0] ;
-input \u_cmos_add/B[4] ;
-input \u_cmos_add/B[3] ;
-input \u_cmos_add/B[2] ;
-input \u_cmos_add/B[1] ;
-input \u_cmos_add/B[0] ;
 input \u_cmos_add/cmos0_vsync ;
 input \u_cmos_add/cmos1_vsync ;
 input \u_cmos_add/pixel_vsync ;
-input \u_cmos_add/pixel_flip_flag ;
-input \u_cmos_add/pixel_href_cnt[9] ;
-input \u_cmos_add/pixel_href_cnt[8] ;
-input \u_cmos_add/pixel_href_cnt[7] ;
-input \u_cmos_add/pixel_href_cnt[6] ;
-input \u_cmos_add/pixel_href_cnt[5] ;
-input \u_cmos_add/pixel_href_cnt[4] ;
-input \u_cmos_add/pixel_href_cnt[3] ;
-input \u_cmos_add/pixel_href_cnt[2] ;
-input \u_cmos_add/pixel_href_cnt[1] ;
-input \u_cmos_add/pixel_href_cnt[0] ;
 input sys_clk;
 input tms_pad_i;
 input tck_pad_i;
@@ -273,36 +219,9 @@ wire \u_cmos_add/cmos0_delay[2] ;
 wire \u_cmos_add/cmos0_delay[1] ;
 wire \u_cmos_add/cmos0_delay[0] ;
 wire \u_cmos_add/cmos0_href_reg ;
-wire \u_cmos_add/R[4] ;
-wire \u_cmos_add/R[3] ;
-wire \u_cmos_add/R[2] ;
-wire \u_cmos_add/R[1] ;
-wire \u_cmos_add/R[0] ;
-wire \u_cmos_add/G[5] ;
-wire \u_cmos_add/G[4] ;
-wire \u_cmos_add/G[3] ;
-wire \u_cmos_add/G[2] ;
-wire \u_cmos_add/G[1] ;
-wire \u_cmos_add/G[0] ;
-wire \u_cmos_add/B[4] ;
-wire \u_cmos_add/B[3] ;
-wire \u_cmos_add/B[2] ;
-wire \u_cmos_add/B[1] ;
-wire \u_cmos_add/B[0] ;
 wire \u_cmos_add/cmos0_vsync ;
 wire \u_cmos_add/cmos1_vsync ;
 wire \u_cmos_add/pixel_vsync ;
-wire \u_cmos_add/pixel_flip_flag ;
-wire \u_cmos_add/pixel_href_cnt[9] ;
-wire \u_cmos_add/pixel_href_cnt[8] ;
-wire \u_cmos_add/pixel_href_cnt[7] ;
-wire \u_cmos_add/pixel_href_cnt[6] ;
-wire \u_cmos_add/pixel_href_cnt[5] ;
-wire \u_cmos_add/pixel_href_cnt[4] ;
-wire \u_cmos_add/pixel_href_cnt[3] ;
-wire \u_cmos_add/pixel_href_cnt[2] ;
-wire \u_cmos_add/pixel_href_cnt[1] ;
-wire \u_cmos_add/pixel_href_cnt[0] ;
 wire sys_clk;
 wire tms_pad_i;
 wire tck_pad_i;
@@ -377,7 +296,7 @@ gw_con_top  u_icon_top(
 
 ao_top u_ao_top(
     .control(control0[9:0]),
-    .data_i({\u_cmos_add/cmos0_data[15] ,\u_cmos_add/cmos0_data[14] ,\u_cmos_add/cmos0_data[13] ,\u_cmos_add/cmos0_data[12] ,\u_cmos_add/cmos0_data[11] ,\u_cmos_add/cmos0_data[10] ,\u_cmos_add/cmos0_data[9] ,\u_cmos_add/cmos0_data[8] ,\u_cmos_add/cmos0_data[7] ,\u_cmos_add/cmos0_data[6] ,\u_cmos_add/cmos0_data[5] ,\u_cmos_add/cmos0_data[4] ,\u_cmos_add/cmos0_data[3] ,\u_cmos_add/cmos0_data[2] ,\u_cmos_add/cmos0_data[1] ,\u_cmos_add/cmos0_data[0] ,\u_cmos_add/cmos0_pclk ,\u_cmos_add/cmos0_href ,\u_cmos_add/cmos1_data[15] ,\u_cmos_add/cmos1_data[14] ,\u_cmos_add/cmos1_data[13] ,\u_cmos_add/cmos1_data[12] ,\u_cmos_add/cmos1_data[11] ,\u_cmos_add/cmos1_data[10] ,\u_cmos_add/cmos1_data[9] ,\u_cmos_add/cmos1_data[8] ,\u_cmos_add/cmos1_data[7] ,\u_cmos_add/cmos1_data[6] ,\u_cmos_add/cmos1_data[5] ,\u_cmos_add/cmos1_data[4] ,\u_cmos_add/cmos1_data[3] ,\u_cmos_add/cmos1_data[2] ,\u_cmos_add/cmos1_data[1] ,\u_cmos_add/cmos1_data[0] ,\u_cmos_add/cmos1_pclk ,\u_cmos_add/cmos1_href ,\u_cmos_add/pixel_data[15] ,\u_cmos_add/pixel_data[14] ,\u_cmos_add/pixel_data[13] ,\u_cmos_add/pixel_data[12] ,\u_cmos_add/pixel_data[11] ,\u_cmos_add/pixel_data[10] ,\u_cmos_add/pixel_data[9] ,\u_cmos_add/pixel_data[8] ,\u_cmos_add/pixel_data[7] ,\u_cmos_add/pixel_data[6] ,\u_cmos_add/pixel_data[5] ,\u_cmos_add/pixel_data[4] ,\u_cmos_add/pixel_data[3] ,\u_cmos_add/pixel_data[2] ,\u_cmos_add/pixel_data[1] ,\u_cmos_add/pixel_data[0] ,\u_cmos_add/pixel_href ,\u_cmos_add/empty ,\u_cmos_add/empty_vsync ,\u_cmos_add/cmos0_delay[10] ,\u_cmos_add/cmos0_delay[9] ,\u_cmos_add/cmos0_delay[8] ,\u_cmos_add/cmos0_delay[7] ,\u_cmos_add/cmos0_delay[6] ,\u_cmos_add/cmos0_delay[5] ,\u_cmos_add/cmos0_delay[4] ,\u_cmos_add/cmos0_delay[3] ,\u_cmos_add/cmos0_delay[2] ,\u_cmos_add/cmos0_delay[1] ,\u_cmos_add/cmos0_delay[0] ,\u_cmos_add/cmos0_href_reg ,\u_cmos_add/pixel_href ,\u_cmos_add/R[4] ,\u_cmos_add/R[3] ,\u_cmos_add/R[2] ,\u_cmos_add/R[1] ,\u_cmos_add/R[0] ,\u_cmos_add/G[5] ,\u_cmos_add/G[4] ,\u_cmos_add/G[3] ,\u_cmos_add/G[2] ,\u_cmos_add/G[1] ,\u_cmos_add/G[0] ,\u_cmos_add/B[4] ,\u_cmos_add/B[3] ,\u_cmos_add/B[2] ,\u_cmos_add/B[1] ,\u_cmos_add/B[0] ,\u_cmos_add/cmos0_vsync ,\u_cmos_add/cmos1_vsync ,\u_cmos_add/pixel_vsync ,\u_cmos_add/pixel_flip_flag ,\u_cmos_add/pixel_href_cnt[9] ,\u_cmos_add/pixel_href_cnt[8] ,\u_cmos_add/pixel_href_cnt[7] ,\u_cmos_add/pixel_href_cnt[6] ,\u_cmos_add/pixel_href_cnt[5] ,\u_cmos_add/pixel_href_cnt[4] ,\u_cmos_add/pixel_href_cnt[3] ,\u_cmos_add/pixel_href_cnt[2] ,\u_cmos_add/pixel_href_cnt[1] ,\u_cmos_add/pixel_href_cnt[0] }),
+    .data_i({\u_cmos_add/cmos0_data[15] ,\u_cmos_add/cmos0_data[14] ,\u_cmos_add/cmos0_data[13] ,\u_cmos_add/cmos0_data[12] ,\u_cmos_add/cmos0_data[11] ,\u_cmos_add/cmos0_data[10] ,\u_cmos_add/cmos0_data[9] ,\u_cmos_add/cmos0_data[8] ,\u_cmos_add/cmos0_data[7] ,\u_cmos_add/cmos0_data[6] ,\u_cmos_add/cmos0_data[5] ,\u_cmos_add/cmos0_data[4] ,\u_cmos_add/cmos0_data[3] ,\u_cmos_add/cmos0_data[2] ,\u_cmos_add/cmos0_data[1] ,\u_cmos_add/cmos0_data[0] ,\u_cmos_add/cmos0_pclk ,\u_cmos_add/cmos0_href ,\u_cmos_add/cmos1_data[15] ,\u_cmos_add/cmos1_data[14] ,\u_cmos_add/cmos1_data[13] ,\u_cmos_add/cmos1_data[12] ,\u_cmos_add/cmos1_data[11] ,\u_cmos_add/cmos1_data[10] ,\u_cmos_add/cmos1_data[9] ,\u_cmos_add/cmos1_data[8] ,\u_cmos_add/cmos1_data[7] ,\u_cmos_add/cmos1_data[6] ,\u_cmos_add/cmos1_data[5] ,\u_cmos_add/cmos1_data[4] ,\u_cmos_add/cmos1_data[3] ,\u_cmos_add/cmos1_data[2] ,\u_cmos_add/cmos1_data[1] ,\u_cmos_add/cmos1_data[0] ,\u_cmos_add/cmos1_pclk ,\u_cmos_add/cmos1_href ,\u_cmos_add/pixel_data[15] ,\u_cmos_add/pixel_data[14] ,\u_cmos_add/pixel_data[13] ,\u_cmos_add/pixel_data[12] ,\u_cmos_add/pixel_data[11] ,\u_cmos_add/pixel_data[10] ,\u_cmos_add/pixel_data[9] ,\u_cmos_add/pixel_data[8] ,\u_cmos_add/pixel_data[7] ,\u_cmos_add/pixel_data[6] ,\u_cmos_add/pixel_data[5] ,\u_cmos_add/pixel_data[4] ,\u_cmos_add/pixel_data[3] ,\u_cmos_add/pixel_data[2] ,\u_cmos_add/pixel_data[1] ,\u_cmos_add/pixel_data[0] ,\u_cmos_add/pixel_href ,\u_cmos_add/empty ,\u_cmos_add/empty_vsync ,\u_cmos_add/cmos0_delay[10] ,\u_cmos_add/cmos0_delay[9] ,\u_cmos_add/cmos0_delay[8] ,\u_cmos_add/cmos0_delay[7] ,\u_cmos_add/cmos0_delay[6] ,\u_cmos_add/cmos0_delay[5] ,\u_cmos_add/cmos0_delay[4] ,\u_cmos_add/cmos0_delay[3] ,\u_cmos_add/cmos0_delay[2] ,\u_cmos_add/cmos0_delay[1] ,\u_cmos_add/cmos0_delay[0] ,\u_cmos_add/cmos0_href_reg ,\u_cmos_add/pixel_href ,\u_cmos_add/cmos0_vsync ,\u_cmos_add/cmos1_vsync ,\u_cmos_add/pixel_vsync }),
     .clk_i(sys_clk)
 );
 
--- src/cmos_add.v
+++ src/cmos_add.v
@@ -21,24 +21,7 @@ module cmos_add(
 );
 
 
-/*
-reg [11:0]      pixel_cnt;//行像素计数器
-reg             pixel_href_reg;
-*/
-
-wire [15:0]     cmos0_data_rd;
-wire            cmos1_write_full;
-
-wire [15:0]     pixel_data_add; //未配准直接加权的像素,存FIFO
-wire [15:0]     pixel_data_add_rd; //FIFO读取用
-wire            pixel_vsync_add; //后半周期场扫描使能，如何控制?
-wire            pixel_data_empty;
-wire            pixel_data_full;
-
-reg [1:0] vsync_add_clk_cnt;
-reg     empty_vsync;
-
-//计数器对系统时钟计数，计时0.2秒
+//计数器对系统时钟计数，计时0.2秒，场信号采样时钟
 reg [23:0] counter;
 reg counter_clk;
 always @(posedge cmos0_pclk or negedge sys_rst_n) begin
@@ -53,21 +36,10 @@ always @(posedge cmos0_pclk or negedge sys_rst_n) begin
         counter_clk <= ~counter_clk;
     end
 end
-/*
-always @(posedge cmos1_pclk or negedge sys_rst_n) begin
-    if(!sys_rst_n)
-        pixel_cnt <= 12'd0;
-    else if(cmos0_href) begin
-        pixel_href_reg <= 1'b1;
-        pixel_cnt <= 12'd0;
-    end
-    else if((!cmos0_href) && pixel_cnt < 12'd640) begin
-        pixel_cnt <= pixel_cnt + 12'd1;
-        pixel_href_reg <= 1'b1;
-    end
-end
-*/
 
+
+wire [15:0]     cmos0_data_rd;
+wire            cmos1_write_full;
 fifo_pong u_fifo_pong(
     .Data       (cmos0_data         ), //input [15:0] Data
     .Reset      (~sys_rst_n         ), //input Reset
@@ -75,32 +47,14 @@ fifo_pong u_fifo_pong(
     .RdClk      (cmos0_pclk         ), //input RdClk
     .WrEn       (cmos0_href         ), //input WrEn
     .RdEn       (~cmos1_href        ), //input RdEn
+//    .RdEn       (~empty        ), //input RdEn
     .Q          (cmos0_data_rd      ), //output [15:0] Q
     .Empty      (empty              ), //output Empty
     .Full       (cmos1_write_full   ) //output Full
 );
 
-//assign pixel_data =  cmos1_data;
-//assign pixel_vsync = cmos0_vsync;//低有效扩充，垂直360原始信号
-
-
-/************2分频后begin**************/
-always @(posedge cmos0_vsync or negedge sys_rst_n) begin
-    if (!sys_rst_n) begin
-        vsync_add_clk_cnt <= 2'b0;
-    end
-    else if (vsync_add_clk_cnt >= 2'd2) begin
-        vsync_add_clk_cnt <= 2'b0;
-    end
-    else begin
-        vsync_add_clk_cnt <= vsync_add_clk_cnt + 2'b1;
-    end
-end
-/****************end*******************/
-
-assign pixel_vsync = (vsync_add_clk_cnt ^ 2'd1) ? 1'b0 : cmos0_vsync ;
-
-/*************将empty信号打到cmos0_pclk里**********/
+//将empty信号打到cmos0_pclk里
+reg empty_vsync;
 reg fifo_empty;
 always @(posedge cmos1_pclk or negedge sys_rst_n) begin
     if(!sys_rst_n) begin
@@ -116,7 +70,6 @@ always @(posedge cmos1_pclk or negedge sys_rst_n) begin
         empty_vsync <= 1'b0;
     end
 end
-/**************************************************/
 
 //对cmos0_href进行计数
 reg  [10:0]     cmos0_href_cnt;
@@ -147,57 +100,76 @@ always @(posedge cmos0_pclk or negedge sys_rst_n) begin
     else if(cmos1_href) cmos0_delay <= 10'd0;
 end
 
-//垂直信号分割
-reg [10:0] pixel_href_cnt;
-reg        pixel_flip_flag;
-always @(posedge pixel_href or negedge sys_rst_n) begin
+
+///cmos_vsync信号2分频作pixel_vsync信号
+reg [1:0] vsync_add_clk_cnt;
+always @(posedge cmos0_vsync or negedge sys_rst_n) begin
     if (!sys_rst_n) begin
-        pixel_href_cnt <= 11'd0;
-        pixel_flip_flag <= 1'b0;
+        vsync_add_clk_cnt <= 2'b0;
     end
-    else if(pixel_href_cnt < 11'd360) begin
-        pixel_href_cnt <= pixel_href_cnt + 1'b1;
-        pixel_flip_flag <= 1'b0;
+    else if (vsync_add_clk_cnt >= 2'd2) begin
+        vsync_add_clk_cnt <= 2'b0;
     end
-    else if(pixel_href_cnt >= 11'd720) begin
-        pixel_href_cnt <= 11'd0;
-        pixel_flip_flag <= 1'b0;
-    end
-    else if(pixel_href_cnt >= 11'd360) begin
-        pixel_href_cnt <= pixel_href_cnt + 1'b1;
-        pixel_flip_flag <= 1'b1;
+    else begin
+        vsync_add_clk_cnt <= vsync_add_clk_cnt + 2'b1;
     end
 end
 
-reg [4:0] R;
-reg [5:0] G;
-reg [4:0] B;
-/*
+assign pixel_vsync = (vsync_add_clk_cnt ^ 2'd1) ? 1'b0 : cmos0_vsync ;
+
+
+
+//cam1行像素点计数
+reg [11:0]      cam1_pixel_cnt;//行像素计数器
+always @(posedge cmos1_pclk or negedge sys_rst_n) begin
+    if (!sys_rst_n)
+        cam1_pixel_cnt <= 12'd0;
+    else if (!cmos1_href) begin
+        cam1_pixel_cnt <= 12'd0;
+    end
+    else if (cam1_pixel_cnt > 12'd639) begin
+        cam1_pixel_cnt <= 12'd0;
+    end
+    else if (cmos1_href && cam1_pixel_cnt < 12'd640) begin
+        cam1_pixel_cnt <= cam1_pixel_cnt + 12'd1;
+    end
+end
+
+localparam PIXEL_OFFSET = 12'd200;
+wire pixel_offset_flag = cam1_pixel_cnt < PIXEL_OFFSET ? 1'b1 : 1'b0;
+
+wire [15:0]     cmos0_data_splicing;
+wire            fifo_splicing_full;
+wire            fifo_splicing_empty;
+fifo_pong u_fifo_splicing(
+    .Data       (cmos0_data         ), //input [15:0] Data
+    .Reset      (~sys_rst_n         ), //input Reset
+    .WrClk      (cmos0_pclk         ), //input WrClk
+    .RdClk      (cmos0_pclk         ), //input RdClk
+    .WrEn       (cmos0_href         ), //input WrEn
+//    .RdEn       (~pixel_offset_flag), //input RdEn
+    .RdEn       (1'b1), //input RdEn
+    .Q          (cmos0_data_splicing), //output [15:0] Q
+    .Empty      (fifo_splicing_empty), //output Empty
+    .Full       (fifo_splicing_full ) //output Full
+);
+
+
+//移位拼接
 always @(*) begin
-    if (!pixel_flip_flag) begin //上半屏
-        pixel_data <= cmos1_href ? cmos1_data : cmos0_data_rd;
-//          pixel_data <= cmos1_href ? 16'd0 : 16'd0;
-    end
-    else if(pixel_flip_flag) begin
-
-//        R <= cmos1_data[14:11] + cmos0_data_rd[14:11];
-//        G <= cmos1_data[9:5]   + cmos0_data_rd[9:5];
-//        B <= cmos1_data[3:0]   + cmos0_data_rd[3:0];
-//        pixel_data <= {R,G,B};
-
-        pixel_data <= cmos1_href ? 16'd0 : 16'd0;
-    end
-end
-*/
-
-//上下分屏显示，用vsync分屏信号做标志
-always @(*) begin
-    pixel_data <= (vsync_add_clk_cnt ^ 2'd1) ?  (cmos1_href ? cmos1_data : cmos0_data_rd) : 16'd0;
+//pixel_data = (vsync_add_clk_cnt ^ 2'd1) ?  (cmos1_href ? cmos1_data : cmos0_data_rd) : 16'd0;
+    case (vsync_add_clk_cnt)//上下分屏显示，用vsync分屏信号做标志
+        2'd0 : begin
+            pixel_data = pixel_offset_flag ? cmos1_data : ~fifo_splicing_empty ? cmos0_data_splicing : 1'b0;
+        end
+        2'd1 : begin
+            pixel_data = cmos1_href ? cmos1_data : cmos0_data_rd;
+        end
+        default : 
+            pixel_data = pixel_offset_flag ? cmos1_data : ~fifo_splicing_empty ? cmos0_data_splicing : 1'b0;
+    endcase
 end
 
-//assign pixel_data = cmos1_href ? cmos1_data : cmos0_data_rd;//场信号后半周期更新行信号
 assign pixel_href =  cmos1_href | (~ empty);
-//assign pixel_href =  cmos0_href_reg|cmos1_href;//保证href信号上升沿对齐第一个像素
-//assign pixel_href =  cmos1_href;
 
 endmodule
